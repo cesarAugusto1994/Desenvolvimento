86615bfdf7df1fcc9830c79d48e1169a0e4df848
Cesar Augusto <cezzaar@gmail.com>
2015-08-27 10:16:08 -0300

    segundo commit

---
+++ src/desenvolvimento/cliente/Pessoa.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cesar
+ * Date: 27/08/15
+ * Time: 09:54
+ */
+
+namespace Desenvolvimento;
+
+
+class Pessoa
+{
+
+}
---
+++ tests/desenvolvimento/cliente/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: cesar
+ * Date: 27/08/15
+ * Time: 09:59
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+
+}
